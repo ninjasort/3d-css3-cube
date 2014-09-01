6c2d551133698cba698e0589547fb822e830f982
Cameron J Roe <cameronjroe@gmail.com>
2014-08-31 20:25:52 -0400

    init

---
+++ classic.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    $('.front').on('click', function () {
+        var track = $('#classic-track').get(0);
+        if ($(this).parent().hasClass('playing')) {
+            track.pause();
+            track.currentTime = 0;
+            $(this).parent().removeClass('playing');
+        } else {
+            $(this).parent().addClass('playing');
+            track.play();
+        }
+    });
+
+});
